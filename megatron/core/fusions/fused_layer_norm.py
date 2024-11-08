444cc3dc59a28349cc32aa4b5c92f8bf407c6991
taidopurason <taido.purason@ut.ee>
2024-11-08 13:43:07 +0200

    Fixed RMSNorm Support

--- megatron/core/fusions/fused_layer_norm.py
+++ megatron/core/fusions/fused_layer_norm.py
@@ -28,7 +28,25 @@ except ImportError:
     HAVE_FUSED_LAYER_NORM = False
 
 
-class FusedLayerNorm(torch.nn.Module):
+class TENorm:
+    """
+    A conditional wrapper to initialize an instance of Transformer-Engine's
+    `LayerNorm` or `RMSNorm` based on input
+    """
+
+    # TODO should we ditch normalization config and just use spec to choose LayerNorm vs RMSNorm?
+    def __new__(cls, config: TransformerConfig, hidden_size: int, eps: float = 1e-5):
+        if config.normalization == "LayerNorm":
+            instance = _FusedLayerNorm(config, hidden_size, eps)
+        elif config.normalization == "RMSNorm":
+            instance = _FusedRMSLayerNorm(config, hidden_size, eps)
+        else:
+            raise Exception('Only LayerNorm and RMSNorm are curently supported')
+
+        return instance
+
+
+class _FusedLayerNorm(torch.nn.Module):
     """Layer Norm, fused into a single CUDA kernel.
 
     Args:
@@ -51,22 +69,22 @@ class FusedLayerNorm(torch.nn.Module):
     """
 
     def __init__(
-            self,
-            config: TransformerConfig,
-            hidden_size: int,
-            eps: float = 1e-5,
+        self,
+        config: TransformerConfig,
+        hidden_size: int,
+        eps: float = 1e-5,
+        persist_layer_norm: bool = True,
+        zero_centered_gamma: bool = False,
+        normalization: str = "LayerNorm",  # included to match TE interface
     ):
         super().__init__()
 
         self.config = config
 
         self.zero_centered_gamma = self.config.layernorm_zero_centered_gamma
-        if self.config.normalization == "LayerNorm":
-            self.norm_fn = FusedLayerNormAffineFunction
-        elif self.config.normalization == "RMSNorm":
-            self.norm_fn = FusedRMSNormAffineFunction
-        else:
-            raise RuntimeError(f'({self.config.normalization}) is not supported in FusedLayerNorm')
+        assert (
+            self.config.normalization == "LayerNorm"
+        ), f'({self.config.normalization}) is not supported in FusedLayerNorm'
 
         # List of hiddens sizes supported in the persistent layer norm kernel
         # If the hidden size is not supported, fall back to the non-persistent
@@ -116,9 +134,6 @@ class FusedLayerNorm(torch.nn.Module):
         self.persist_layer_norm = persist_layer_norm
         self.sequence_parallel = self.config.sequence_parallel
 
-        if persist_layer_norm:
-            warnings.warn("Persistent Layer Norm is not supported in this version of Megatron.")
-
         # set sequence parallelism flag on weight and bias parameters
         setattr(self.weight, 'sequence_parallel', self.sequence_parallel)
         setattr(self.bias, 'sequence_parallel', self.sequence_parallel)
@@ -136,21 +151,156 @@ class FusedLayerNorm(torch.nn.Module):
 
         weight = self.weight + 1 if self.zero_centered_gamma else self.weight
 
-        if (
+        if self.persist_layer_norm:
+            if 'memory_efficient' in inspect.getfullargspec(FastLayerNormFN.forward).args:
+                output = FastLayerNormFN.apply(
+                    input, weight, self.bias, self.eps, self.config.memory_efficient_layer_norm
+                )
+            else:
+                output = FastLayerNormFN.apply(input, weight, self.bias, self.eps)
+
+            # Apex's fast layer norm function outputs a 'view' tensor (i.e., has
+            # a populated '_base' field). This will result in schedule.py's
+            # deallocate_output_tensor() throwing an error, so a viewless tensor is
+            # created to prevent this.
+            output = make_viewless_tensor(
+                inp=output, requires_grad=input.requires_grad, keep_graph=True
+            )
+
+        else:
+            if (
                 'memory_efficient'
                 in inspect.getfullargspec(FusedLayerNormAffineFunction.forward).args
+            ):
+                return FusedLayerNormAffineFunction.apply(
+                    input,
+                    weight,
+                    self.bias,
+                    self.hidden_size,
+                    self.eps,
+                    self.config.memory_efficient_layer_norm,
+                )
+            else:
+                return FusedLayerNormAffineFunction.apply(
+                    input, weight, self.bias, self.hidden_size, self.eps
+                )
+
+        return output
+
+
+class _FusedRMSLayerNorm(torch.nn.Module):
+    """Layer Norm, fused into a single CUDA kernel.
+
+    Args:
+      hidden_size (int): Transformer hidden dimension.
+
+      eps (float): Epsilon added to denominator, for numerical stability.
+
+      persist_layer_norm (bool): Use persistent fused layer norm kernel.
+      This kernel supports only a set of hidden sizes. Please
+      check persist_ln_hidden_sizes if your hidden size is supported.
+
+      zero_centered_gamma (bool): Adjust LayerNorm weights such that they are
+      centered around zero. This improves numerical stability.
+
+      config (TransformerConfig): Transformer config. Include to match custom
+      layer norm interfaces.
+
+      normalization (str): Normalization type, used for Transformer Engine.
+      Must equal 'LayerNorm' here.
+    """
+
+    def __init__(
+            self,
+            config: TransformerConfig,
+            hidden_size: int,
+            eps: float = 1e-5,
+    ):
+        super().__init__()
+
+        self.config = config
+
+        self.zero_centered_gamma = self.config.layernorm_zero_centered_gamma
+        if self.config.normalization != "RMSNorm":
+            raise RuntimeError(f'({self.config.normalization}) is not supported in FusedLayerNorm')
+
+        self.norm_fn = FusedRMSNormAffineFunction
+        # List of hiddens sizes supported in the persistent layer norm kernel
+        # If the hidden size is not supported, fall back to the non-persistent
+        # kernel.
+        persist_ln_hidden_sizes = [
+            1024,
+            1536,
+            2048,
+            2304,
+            3072,
+            3840,
+            4096,
+            5120,
+            6144,
+            8192,
+            10240,
+            12288,
+            12800,
+            15360,
+            16384,
+            18432,
+            20480,
+            24576,
+            25600,
+            30720,
+            32768,
+            40960,
+            49152,
+            65536,
+        ]
+        persist_layer_norm = self.config.persist_layer_norm
+        if hidden_size not in persist_ln_hidden_sizes or not HAVE_PERSIST_LAYER_NORM:
+            persist_layer_norm = False
+
+        if not persist_layer_norm and not HAVE_FUSED_LAYER_NORM:
+            # TODO: Add pytorch only layer norm
+            raise ValueError(f'Apex must be installed to use FusedLayerNorm.')
+
+        if isinstance(hidden_size, numbers.Integral):
+            hidden_size = (hidden_size,)
+        self.hidden_size = torch.Size(hidden_size)
+        self.eps = eps
+        # Parameters need to be initialized with torch.empty rather than torch.Tensor for correct device placement with nemo2.
+        self.weight = Parameter(torch.empty(*hidden_size))
+        self.reset_parameters()
+        self.persist_layer_norm = persist_layer_norm
+        self.sequence_parallel = self.config.sequence_parallel
+
+        if persist_layer_norm:
+            warnings.warn("Persistent Layer Norm is not supported in this version of Megatron.")
+
+        # set sequence parallelism flag on weight and bias parameters
+        setattr(self.weight, 'sequence_parallel', self.sequence_parallel)
+
+    def reset_parameters(self):
+
+        if self.zero_centered_gamma:
+            init.zeros_(self.weight)
+        else:
+            init.ones_(self.weight)
+
+    def forward(self, input: Tensor) -> Tensor:
+
+        weight = self.weight + 1 if self.zero_centered_gamma else self.weight
+
+        if (
+                'memory_efficient'
+                in inspect.getfullargspec(self.norm_fn.forward).args
         ):
             return self.norm_fn.apply(
                 input,
                 weight,
-                self.bias,
                 self.hidden_size,
                 self.eps,
                 self.config.memory_efficient_layer_norm,
             )
         else:
             return self.norm_fn.apply(
-                input, weight, self.bias, self.hidden_size, self.eps
+                input, weight, self.hidden_size, self.eps
             )
-
-        return output
