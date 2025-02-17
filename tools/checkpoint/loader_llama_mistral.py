5ada01065b7b01cc5da340912703e9b85e17695c
taidopurason <taido.purason@ut.ee>
2025-02-17 16:33:14 +0200

    Added tied embedding support

--- tools/checkpoint/loader_llama_mistral.py
+++ tools/checkpoint/loader_llama_mistral.py
@@ -334,7 +334,8 @@ def set_preprocess_state(args, model, hf_model):
 def set_postprocess_state(args, model, hf_model):
     '''Set output layer & norm params.'''
     model.language_model.encoder.final_norm.weight.data.copy_(hf_model.model.norm.weight)
-    model.language_model.output_layer.weight.data.copy_(hf_model.lm_head.weight)
+    if args.untie_embeddings_and_output_weights:
+        model.language_model.output_layer.weight.data.copy_(hf_model.lm_head.weight)
 
 
 def set_attn_state(args, layer, hf_layer):
